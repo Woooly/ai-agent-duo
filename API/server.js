d826e1f43fd91d79105f8e0af5cc354f6897f4f5
anapaulalgb <ana_paula7@outlook.com.br>
2025-11-07 16:48:33 -0300

    Adiciona projeto inicial DEV RECEITA

---
+++ API/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import receitasRoutes from './src/route/receitas.route.js'
+
+const app = express ()
+const PORT = 3001
+
+app.use(express.json())
+
+app.use('/api/receitas', receitasRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando na Porta ${PORT}`)
+
+})
+  
