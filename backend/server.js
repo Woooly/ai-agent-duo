f7aa05a0eebc24d1b173247313d70be2c06dde75
Woooly <gabriel.marinho.programmer@gmail.com>
2025-12-09 13:11:44 -0300

    Cors fix

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import receitasRoutes from './src/route/receitas.route.js'
 import cors from 'cors'
 
 const app = express ()
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
     
