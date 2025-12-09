1c09ff6f885e7d64708539afe48f8812ff2eaddc
Woooly <gabriel.marinho.programmer@gmail.com>
2025-12-09 12:52:27 -0300

    Finished

--- API/server.js
+++ API/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import receitasRoutes from './src/route/receitas.route.js'
+import cors from 'cors'
 
 const app = express ()
 const PORT = 3001
+
+app.use(cors({}))
 
 app.use(express.json())
 
---
+++ interface-receitas/src/pages/ChatReceitas.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import ListaMensagens from "../components/ListaMensagens";
+import ChatBox from "../components/ChatBox";
+import { api } from "../services/api";
+
+const ChatReceitas = () => {
+    const [loading, setLoading] = useState(false)
+    const [mensagens, setMensagens] = useState([
+    {
+      id: 1,
+      texto: "Olá! Sou seu assistente de receitas. Como posso ajudar você hoje?",
+      remetente: "bot",
+    }
+])
+
+    const onEnviarMensagem = async (mensagem) => {
+      const novaMensagemUsuario = {
+        id: Date.now(),
+        texto: mensagem,
+        remetente: "usuario"
+      }
+
+      setMensagens(prev => [...prev, novaMensagemUsuario] )
+      setLoading(true)
+
+        try {
+            const resposta = await api(mensagem)
+            const novaMensagemBot = {
+              id: Date.now() + 1,
+              texto: resposta,
+              remetente: "bot"
+            }
+
+            setMensagens(prev => [...prev, novaMensagemBot])
+        }catch(err){
+            console.error(err)
+        }finally{
+          setLoading(false)
+        }
+  }
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-500 via-gray-50 to-emerald-50 p-4">
+      <div className="container mx-auto max-w-4xl">
+        <header className="text-center mb-8">
+          <h1 className="text-5xl font-bold bg-black text-transparent bg-clip-text mb-2">
+            🍽 Chef
+          </h1>
+          <p className="text-black text-lg">
+            Seu assistente pessoal para receitas deliciosas
+          </p>
+        </header>
+
+        <div className="bg-white/70 backdrop-blur-sm rounded-2xl overflow-hidden shadow-xl h-[600px] border border-gray-100 flex flex-col">
+                <ListaMensagens mensagens={mensagens} />
+                <ChatBox onEnviarMensagem={onEnviarMensagem} desabilitado={loading}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatReceitas;
