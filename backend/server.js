2f33d5da0a518ca994140b82fbf2adda103dc969
Woooly <gabriel.marinho.programmer@gmail.com>
2025-12-09 13:07:31 -0300

    Cors fix

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,16 @@ import cors from 'cors'
 const app = express ()
 const PORT = 3001
 
+const corsOptions = {
+    
+    origin: [
+        'http://localhost:5173',         
+        'https://ai-agent-duo.vercel.app' 
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true 
+};
+
 app.use(cors({}))
 
 app.use(express.json())
